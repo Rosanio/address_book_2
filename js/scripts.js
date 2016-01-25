a2e0cf47ab06d619bb7fe4a2d01a51f98c22f26d
Anna Becker and Jordan Meier <jbirdmeier@gmail.com>
2016-01-25 08:50:23 -0800

    Add user logic interface by incorporating submit event handler which also appends user input to the index.html

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,26 @@ function Contact(firstName, lastName) {
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+
+    var inputFirstName = $("input#new-first-name").val();
+    var inputLastName = $("input#new-last-name").val();
+    var newContact = new Contact(inputFirstName, inputLastName);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>")
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+
+  event.preventDefault();
+  });
+
+  $(".contact").last().click(function() {
+  $("#show-contact").show();
+  $("#show-contact h2").text(newContact.fullName());
+  $(".first-name").text(newContact.firstName);
+  $(".last-name").text(newContact.lastName);
+  });
+});
